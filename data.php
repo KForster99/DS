49c254715c2797aea540840350a5208246c9ad44
ont <tairat-n>
2021-10-07 22:15:08 +0700

    finish

--- data.php
+++ data.php
@@ -1,61 +1,45 @@
 <?php
-error_reporting(E_ERROR | E_PARSE);
-$en_text = "ATTACKATDAWN";
-$de_text = "FBGBF";
-$secret  = "LEMONLEMONLE";
+$en_text = "HELLODFGHsd5115";
+$de_text = "!qnvzmw|l[ULvAu";
+$secret  = "XMCKLJRVEgp8f1a";
+
+//$en_text = "ANT";
+//$de_text = "FBG";
+//$secret = "FMV";
 
 function encrypt($text, $secret)
 {
-    echo $text . "<br>";
-    echo $secret . "<br>";
     for ($i = 0; $i < strlen($text); $i++) {
-        $text_int[$i] = ord("$text[$i]");
-        $secret_int[$i] = ord("$secret[$i]");
-        echo "ord: " . $text_int[$i] . " / " . $secret_int[$i] . "<br>";
-
-        $text_int[$i] = $text_int[$i] - 65;
-        $secret_int[$i] = $secret_int[$i] - 65;
-        echo "-65: " . $text_int[$i] . " / " . $secret_int[$i] . "<br>";
-
-        $text_bi[$i] = decbin($text_int[$i]);
-        $secret_bi[$i] = decbin($secret_int[$i]);
-        echo "decbin: " . $text_bi[$i] . " / " . $secret_bi[$i] . "<br>";
-
-        // $int_text = (int)$text_bi[$i];
-        // $int_secret = (int)$secret_bi[$i];
-        // echo "(int)decbin: " . $int_text . " / " . $int_secret . "<br>";
-
-        $xor[$i] = intval($text_bi[$i]) ^ intval($secret_bi[$i]);
-        // echo $int_text ^ $int_secret . "<br>";
-        echo "xor: " . $xor[$i]  . "<br>";
-
-        $text_newint[$i] = bindec($xor[$i]) + 65;
-        echo "bindec: " . $text_newint[$i] . "<br>";
-
-        $text_new[$i] = chr($text_newint[$i]);
-        echo  $text_new[$i] . "<br><br>";
+        $array_text[$i] = $text[$i];
+        $array_secret[$i] = $secret[$i];
+        $text_int[$i] = ord($array_text[$i]) - 33;
+        $secret_int[$i] = ord($array_secret[$i]) - 33;
+        $text_secret[$i] = $text_int[$i] + $secret_int[$i];
+        $text_secret[$i] = ($text_secret[$i] % 94) + 33;
+        $new_text[$i] = chr($text_secret[$i]);
+        echo $new_text[$i] . "";
     }
 }
+//echo enencrypt($en_text, $arr_secret) . "          ";
 
 function decrypt($text, $secret)
 {
     for ($j = 0; $j < strlen($text); $j++) {
         $array_text[$j] = $text[$j];
         $array_secret[$j] = $secret[$j];
-        $text_int[$j] = ord($array_text[$j]);
-        $secret_int[$j] = ord($array_secret[$j]);
-        $text_int[$j] = $text_int[$j] - 65;
-        $secret_int[$j] = $secret_int[$j] - 65;
-        $text_bi[$j] = decbin($text_int[$j]);
-        $secret_bi[$j] = decbin($secret_int[$j]);
-        $int_text = (int)$text_bi[$j];
-        $int_secret = (int)$secret_bi[$j];
-        $xor[$j] = $int_text ^ $int_secret;
-        $text_newint[$j] = bindec($xor[$j]) + 65;
-        $text_new[$j] = chr($text_newint[$j]);
-        echo $text_new[$j];
+        $text_int[$j] = ord($array_text[$j]) - 33;
+        $secret_int[$j] = ord($array_secret[$j]) - 33;
+        if ($text_int[$j] - $secret_int[$j] < 0) {
+            $text_int[$j] = $text_int[$j] + 94;
+        }
+        $text_secret[$j] = $text_int[$j] - $secret_int[$j];
+        $text_secret[$j] = ($text_secret[$j] % 94) + 33;
+        $new_text[$j] = chr($text_secret[$j]);
+        echo $new_text[$j] . "";
     }
 }
+//echo "en_text=ATTACKATDAWN || secret=LEMONLEMONLE" . "<br>";
 encrypt($en_text, $secret);
-echo "<br>";
+echo "<br> <br>";
+//echo "de_text=LXAOCBEAHNAC || secret=LEMONLEMONLE" . "<br>";
 decrypt($de_text, $secret);
